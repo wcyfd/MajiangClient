1d695c65cb07b9063e559e2cb040194771cd5b5d
wcyfd <1101697681@qq.com>
2017-09-04 19:03:49 +0800

    create

---
+++ src/com/randioo/majiang_collections_client/MajiangClient.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.randioo.majiang_collections_client;
+
+/**
+ *
+ * @author asus
+ */
+public class MajiangClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
